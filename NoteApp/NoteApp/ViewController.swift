136e02496938ee55981cd7bb78124eab57a04b93
Bermet Toichubekova <toichubekovabermet@gmail.com>
2023-11-28 16:59:04 +0600

    Note App

---
+++ NoteApp/NoteApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by Bermet Toichubekova on 28/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
